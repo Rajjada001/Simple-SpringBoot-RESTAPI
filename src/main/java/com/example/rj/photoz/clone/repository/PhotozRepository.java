98eeb85e6e624bc3f963d00b47f8c98deee67429
Raj Jada <Rajjada001@gmail.com>
2025-02-24 13:50:07 -0500

    Initial commit

---
+++ src/main/java/com/example/rj/photoz/clone/repository/PhotozRepository.java
@@ -0,0 +1,8 @@
+package com.example.rj.photoz.clone.repository;
+
+import com.example.rj.photoz.clone.model.Photo;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PhotozRepository extends CrudRepository<Photo, Integer> {
+
+}
